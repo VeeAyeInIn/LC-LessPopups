732faef6156d331c2100e152c5deaabf69b454ec
VeeAyeInIn <40581838+VeeAyeInIn@users.noreply.github.com>
2024-10-04 17:23:54 -0400

    Delete LessPopups/obj directory

--- LessPopups/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LessPopups
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "Vee.LessPopups";
-        public const string PLUGIN_NAME = "LessPopups";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
